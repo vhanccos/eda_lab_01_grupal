70d72480e7811a342a68d79b1a32a8d4b5dc0591
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-05-07 15:25:16 -0500

    Métodos en Java completado

---
+++ Methods.java
@@ -0,0 +1,9 @@
+public class Methods {
+    static void myMethod() {
+        System.out.println("¡Se acaba de ejecutar!");
+    }
+
+    public static void main(String[] args) {
+        myMethod(); //invocamos el método myMethod
+    }
+}
