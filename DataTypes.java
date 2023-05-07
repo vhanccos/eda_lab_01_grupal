31393f28b255d42d3d00cf2019aafd272c878094
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-05-07 14:57:32 -0500

    Tipos de Datos en Java completado

---
+++ DataTypes.java
@@ -0,0 +1,9 @@
+public class DataTypes {
+  public static void main(String[] args) {
+    int myNum = 9;
+    float myFloatNum = 8.99f;
+    char myLetter = 'A';
+    boolean myBool = false;
+    String myText = "Hello World";
+  }
+}
