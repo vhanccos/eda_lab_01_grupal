2e8c8dfca6f2cf342584ba9a0ca2c01bbedc5cde
carrascoArles <acarrascoch@unsa.edu.pe>
2023-05-06 21:28:28 -0500

    los 4 ejercicios del for

---
+++ forloop.java
@@ -0,0 +1,27 @@
+public class forloops{
+		public static void main(String args[]){
+
+	//ejer 3
+for(int i = 0; i < 5; i++) {
+	  System.out.println("Yes");
+}
+	//ejer 4
+String[] cars={"volvo","BMW","Ford"};
+	for(String i : cars){
+	System.out.println(i);
+}
+        //ejer5
+for (int i = 0; i < 10; i++) {
+	  if (i == 5) {
+            break;}
+	    System.out.println(i);
+}
+        //ejer6
+for (int i = 0; i < 10; i++) {
+	  if (i == 4) {
+	    continue;}
+	    System.out.println(i);
+}
+
+}}
+
