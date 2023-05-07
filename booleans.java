23c9f823dc09eb6e8d2b85e125fc457d89ebf55e
carrascoArles <acarrascoch@unsa.edu.pe>
2023-05-06 20:49:52 -0500

    los 2 ejercicios booleanos

---
+++ booleans.java
@@ -0,0 +1,13 @@
+public class Booleans {
+	    public static void main(String[] args) {
+		            
+            boolean isJavaFun = true;
+	    boolean isFishTasty = false;
+	    System.out.println(isJavaFun);
+	    System.out.println(isFishTasty);
+
+	    int x = 10;
+	    int y = 9;
+	    System.out.println(x > y);
+				        }
+}
