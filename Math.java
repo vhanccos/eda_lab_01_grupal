439a4a69d50469f333f77e2a0df40cebb1f72f57
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-05-07 15:17:49 -0500

    Métodos matemáticos en Java completado

---
+++ Math.java
@@ -0,0 +1,15 @@
+public class Math {
+  public static void main(String[] args) {
+    // ejercicio 1
+    int x = 5;
+    int y = 10;
+    int maximo = Math.max(x, y); //maximo entre 5 y 10
+
+    // ejercicio 2
+    x = 16;
+    int raiz = Math.sqrt(x); //rqaiz cuadrada de 16
+
+    // ejercicio 3
+    int aleat = Math.random(); //aleatorio entre 0 y 1
+  }
+}
