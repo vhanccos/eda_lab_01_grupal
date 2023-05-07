267ebf82a6c9cfb166e873ecabc777be0d1dfdb5
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-05-07 15:06:41 -0500

    Agregados los ejercicios 2 y 3

--- DataTypes.java
+++ DataTypes.java
@@ -1,9 +1,18 @@
 public class DataTypes {
   public static void main(String[] args) {
+    // ejercicio 1
     int myNum = 9;
     float myFloatNum = 8.99f;
     char myLetter = 'A';
     boolean myBool = false;
     String myText = "Hello World";
+
+    // ejercicio 2
+    // byte, short, int, long, float, double, boolean and char son tipos de datop
+    // primitivos.
+
+    // ejercicio 3
+    double myDouble = 9.78d;
+    int myInt = (int) myDouble;
   }
 }
