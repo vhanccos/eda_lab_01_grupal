5490dace6ea0a982c27cec318df7f8dfc25896ee
YoelLeon <132792733+YoelLeon@users.noreply.github.com>
2023-05-07 17:20:15 -0400

    variables

---
+++ java_variables.java
@@ -0,0 +1,30 @@
+public class Variables {
+
+    public static void main(String[] args) {
+       
+        //ejercicio 1
+        String carName="Volvo";
+        System.out.println(carName);
+        
+        //ejercicoi 2
+        
+        int maxSpeed=120;
+        System.out.println(maxSpeed);
+        
+        //ejercicio 3
+        int x=5;
+        int y=10;
+        System.out.println(x+y);
+
+	// ejercicio 4
+            
+            int z=x+y;
+            System.out.println(z);
+            
+        //ejercicio 5
+
+            int x=5,y=6,Z=50;
+        
+    }
+    
+}
