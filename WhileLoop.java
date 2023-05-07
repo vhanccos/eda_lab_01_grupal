3a859e7da88ca88e7eeb56ae3f7d83c2106f4bee
Vladimir Jaward Hancco Soncco <vhanccos@unsa.edu.pe>
2023-05-07 15:21:16 -0500

    Bucle While en Java completado

---
+++ WhileLoop.java
@@ -0,0 +1,10 @@
+public class WhileLoop {
+  public static void main(String[] args) {
+    // ejercicio 1
+    int i = 1;
+    while (i < 6) {
+      System.out.println(i); //muestra numeros del 1 al 5
+      i++;
+    }
+  }
+}
