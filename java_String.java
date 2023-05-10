398c86d9c23f76d702a5d73694ce0c8864e25189
YoelLeon <132792733+YoelLeon@users.noreply.github.com>
2023-05-10 14:29:10 -0400

    Se agrego 3 ejercicios mas

--- java_String.java
+++ java_String.java
@@ -1,5 +1,15 @@
 public class JavaString {
     public static void main(String[] args) {
+	//Ejercicio 1
+        String greeting="Hello";
+        
+        //Ejercicio 2
+        String txt="Hello";
+        System.out.println(txt.length());
+       
+        //ejercicio 3
+        System.out.println(txt.toUpperCase());
+        
 
     } 
 }
