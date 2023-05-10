e6199759a73a0fb173d129222e3b837a3403496e
YoelLeon <132792733+YoelLeon@users.noreply.github.com>
2023-05-10 14:34:13 -0400

    Se agrego el primer ejercicio

--- java_Switch.java
+++ java_Switch.java
@@ -1,6 +1,17 @@
 public class JavaSwitch {
 
     public static void main(String[] args) {
+	 //Ejercicio 1
+       int day=2;
+       switch(day){
+           case 1:
+               System.out.println("Saturday");
+               break;
+           case 2:
+               System.out.println("Sunday");
+               break;
+       }
+       
 
 
     }
