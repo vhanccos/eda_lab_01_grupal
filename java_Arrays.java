5cb2e9edc3adacaf0750e2be141081f8dfbc288c
YoelLeon <132792733+YoelLeon@users.noreply.github.com>
2023-05-10 14:42:05 -0400

    Se agrego los ejercicios restantes

--- java_Arrays.java
+++ java_Arrays.java
@@ -9,6 +9,15 @@ public class JavaArrays {
         //Ejercicio 3
         cars[0]="Opel";
         System.out.println(cars[0]);
+	//Ejercicio 4
+        System.out.println(cars.length);
+
+        //Ejercicio 5
+        for(String i:cars){
+            System.out.println(i);
+        }
+        //Ejercicio 6
+        int[][]myNumbers={{1,2,3,4},{5,6,7}};
 
 
 	    } 
