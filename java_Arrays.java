3dba7ee49693829617ad4959d454db7dae5950d1
YoelLeon <132792733+YoelLeon@users.noreply.github.com>
2023-05-10 14:41:07 -0400

    Se realizo los 3 primeros ejercicios

--- java_Arrays.java
+++ java_Arrays.java
@@ -1,5 +1,14 @@
 public class JavaArrays {
     public static void main(String[] args) {
+	 //Ejercicio 1
+        String [] cars={"volvo","BWM","Ford"};
+        
+        //Ejercicio 2
+        System.out.println(cars[1]);
+        
+        //Ejercicio 3
+        cars[0]="Opel";
+        System.out.println(cars[0]);
 
 
 	    } 
